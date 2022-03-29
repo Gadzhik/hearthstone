cdbc95e560f4c417a6f88038b0e3930da54a0bd3
Gadzhik <gkurban@outlook.com>
2022-03-29 22:38:53 +0200

    first commit

---
+++ hearthstone/android/app/src/main/kotlin/com/example/hearthstone/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.hearthstone
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ hearthstone/lib/widgets/auth/auth_widget.dart
@@ -0,0 +1,16 @@
+// Для сохранения данных будем использовать StateFull widget
+import 'package:flutter/material.dart';
+
+class AuthWidget extends StatefulWidget {
+  const AuthWidget({Key? key}) : super(key: key);
+
+  @override
+  State<AuthWidget> createState() => _AuthWidgetState();
+}
+
+class _AuthWidgetState extends State<AuthWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
